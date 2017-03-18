37154a9d016ec05a7a99127846c48e591693d90b
letanphuc <letanphuc170296@gmail.com>
2017-03-18 14:28:18 +0700

    them

--- QLXuatNhapKhoLKDT/Form/Danh Muc/XtraForm_LKDT.cs
+++ QLXuatNhapKhoLKDT/Form/Danh Muc/XtraForm_LKDT.cs
@@ -8,16 +8,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using QLXuatNhapKhoLKDT.Classes.DanhMuc;
+
 
 namespace QLXuatNhapKhoLKDT.Form.Danh_Muc
 {
     public partial class XtraForm_LKDT : DevExpress.XtraEditors.XtraForm
     {
+        ConnectToDatabase Data;
+        ClassLinhKien clk; //Khai báo biến kiểu ClassLinhKien
         public XtraForm_LKDT()
         {
             InitializeComponent();
+            clk = new ClassLinhKien(); //Khởi tạo biến lấy dữ liệu của class Linh kiện
+            Data = new ConnectToDatabase();
         }
-
+ 
         private void simpleButton3_Click(object sender, EventArgs e)
         {
             XtraForm_LoaiLKDT form = new XtraForm_LoaiLKDT();
@@ -27,6 +33,34 @@ namespace QLXuatNhapKhoLKDT.Form.Danh_Muc
             {
                 this.Enabled = true;
             }
+        }
+
+        private void XtraForm_LKDT_Load(object sender, EventArgs e)
+        {
+            clk.LoadData(this); //Load dữ liệu lên from
+        }
+        
+
+        private void btLuu_Click(object sender, EventArgs e)
+        {
+            clk.ThemLK(comboBoxEdit2.SelectedItem.ToString(), comboBoxEdit3.SelectedItem.ToString(), textEdit1.Text,dateTimePicker1.Value, textEdit4.Text, Int32.Parse(textEdit2.Text), false, pictureEdit1.Text, comboBoxEdit1.SelectedItem.ToString());
+            clk.LoadData(this);
+        }
+
+        private void btChinhSua_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void gridView1_RowClick(object sender, DevExpress.XtraGrid.Views.Grid.RowClickEventArgs e)
+        {
+            var lk = gridView1.GetFocusedRowCellValue("MA_LK").ToString();
+            
         }
     }
 }
